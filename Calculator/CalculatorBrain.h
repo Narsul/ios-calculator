62f58735f0a57860c7aa5414f2e93000cfeef3f2
Narsul <antony.hell@narsul.in.ua>
2012-11-18 00:13:20 +0200

    * Basic operations * Backspace functionality

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Antony Hell on 17.11.12.
+//  Copyright (c) 2012 Antony Hell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+
+@end
--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,16 +7,75 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController ()
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic) BOOL dotAlreadyExistsInANumber;
+@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
 @implementation CalculatorViewController
 
+- (CalculatorBrain *)brain {
+    if (!_brain) _brain = [[CalculatorBrain alloc] init];
+    return _brain;
+}
+
 - (IBAction)digitPressed:(UIButton *)sender {
     NSString *digit = sender.currentTitle;
-    self.display.text = [self.display.text stringByAppendingString:digit];
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        self.display.text = [self.display.text stringByAppendingString:digit];
+    } else {
+        self.display.text = digit;
+        self.userIsInTheMiddleOfEnteringANumber = YES;
+    }
+}
+
+- (IBAction)enterPressed {
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+    self.dotAlreadyExistsInANumber =  NO;
+}
+
+- (IBAction)dotPressed {
+    if (self.dotAlreadyExistsInANumber) return;
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        self.display.text = [self.display.text stringByAppendingString:@"."];
+    } else {
+        self.display.text = @"0.";
+        self.userIsInTheMiddleOfEnteringANumber = YES;
+    }
+    self.dotAlreadyExistsInANumber = YES;
+}
+
+- (IBAction)operationPressed:(id)sender {
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        [self enterPressed];
+    }
+    NSString *operation = [sender currentTitle];
+    double result = [self.brain performOperation:operation];
+    self.display.text = [NSString stringWithFormat:@"%g", result];
+}
+
+- (IBAction)backspacePressed {
+    if (!self.userIsInTheMiddleOfEnteringANumber) return;
+    
+    NSUInteger displayLength = [self.display.text length];
+    NSString *charToBeDeleted = [self.display.text substringFromIndex:(displayLength-1)];
+    NSString *charBeforeCharToBeDeleted = [[self.display.text substringFromIndex:(displayLength-2)] substringToIndex:(displayLength-1)];
+    if (displayLength == 1) {
+        self.display.text = @"0";
+        self.userIsInTheMiddleOfEnteringANumber = NO;
+    } else {
+        if ([charToBeDeleted isEqualToString:@"."]) {
+            self.dotAlreadyExistsInANumber = NO;
+            if ([charBeforeCharToBeDeleted isEqualToString:@"0"]) {
+                self.userIsInTheMiddleOfEnteringANumber = NO;
+            }
+        }
+        self.display.text = [self.display.text substringToIndex:(displayLength-1)];
+    }
 }
 
 @end
