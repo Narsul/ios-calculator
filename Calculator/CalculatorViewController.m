4d54e2dbb6c16c92db6c969b18e55f2a491a72c8
Antony Peklo <antony.hell@me.com>
2012-11-19 10:35:58 +0200

    "=" removed every time history is being updated

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -107,10 +107,8 @@
 }
 
 - (void)addToHistory:(NSString *)historyItem {
-    // Delete " =" from history display string if current historyItem is a "="
-    if ([historyItem isEqualToString:@"="]) {
-        self.historyDisplay.text = [self.historyDisplay.text stringByReplacingOccurrencesOfString:@" =" withString:@""];
-    }
+    // Delete " =" from history display string
+    self.historyDisplay.text = [self.historyDisplay.text stringByReplacingOccurrencesOfString:@" =" withString:@""];
     
     // Append new item to history with prepending space symbol
     self.historyDisplay.text = [self.historyDisplay.text stringByAppendingString:[historyItem stringByAppendingString:@" "]];
